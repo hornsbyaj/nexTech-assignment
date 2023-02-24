b8aedca1863504121b67ef23921930fc5d80625c
antonio.hornsby@go.tarleton.edu <antonio.hornsby@go.tarleton.edu>
2023-02-23 20:16:22 -0600

    removed counter component, modified home, nav, and news feed html.

--- hacker-news-webApplication/ClientApp/src/app/app.module.ts
+++ hacker-news-webApplication/ClientApp/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-//import { MatInputModule } from "@angular/material/input";
-
+//import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
-//import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-//import { MatCardModule } from "@angular/material/card";
 
 
 @NgModule({
@@ -20,7 +16,6 @@ import { NewsFeedComponent } from './news-feed/news-feed.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     FetchDataComponent,
     NewsFeedComponent
   ],
@@ -28,12 +23,9 @@ import { NewsFeedComponent } from './news-feed/news-feed.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     FormsModule,
     HttpClientModule,
-    //BrowserAnimationsModule,
-    //MatCardModule,
-    //MatInputModule,
+    //NgxPaginationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'news-feed', component: NewsFeedComponent },
     ])
